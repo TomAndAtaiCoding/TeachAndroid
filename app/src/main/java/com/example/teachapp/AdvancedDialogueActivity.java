2055a82c4cf3d7d88baab033dfb222ef5f33f857
Tom Dotan <dotantom1@gmail.com>
2020-10-01 13:25:40 -0400

    feat(app): start activities
    Added layout and class files for each activity

---
+++ app/src/main/java/com/example/teachapp/AdvancedDialogueActivity.java
@@ -0,0 +1,14 @@
+package com.example.teachapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdvancedDialogueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advanced_dialogue);
+    }
+}

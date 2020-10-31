83c4a9a13b07b7e653725682062945b06786c98e
Atai Ambus <atayambus@gmail.com>
2020-10-31 17:13:41 +0200

    tmp
    - Change 1
    
    - Change 2
    
    - Change 3
    
    footer

--- app/src/main/java/com/example/teachapp/ListViewActivity.java
+++ app/src/main/java/com/example/teachapp/ListViewActivity.java
@@ -3,12 +3,39 @@ package com.example.teachapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class ListViewActivity extends AppCompatActivity {
+
+    EditText m_editList;
+    Button m_submitList;
+    ListView m_listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview);
+
+        m_editList = findViewById(R.id.edit_list);
+        m_submitList = findViewById(R.id.button_submit_conversation);
+
+        m_submitList.setOnClickListener((v) -> {
+            m_listView = new ListView(this);
+            m_listView.setAdapter(new ArrayAdapter<String>(
+                    this,
+                    R.layout.listrow_listview_example,
+                    ",".split(String.valueOf(m_editList.getText()))));
+
+
+        });
     }
+
+
+
+
+
+
 }

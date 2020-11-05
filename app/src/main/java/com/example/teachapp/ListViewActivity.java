e06599f2dad07d5a16021c595e9a91c6fe9f6134
Atai Ambus <atayambus@gmail.com>
2020-11-05 13:05:40 +0200

    feat(): add ListView activity

--- app/src/main/java/com/example/teachapp/ListViewActivity.java
+++ app/src/main/java/com/example/teachapp/ListViewActivity.java
@@ -11,26 +11,45 @@ import android.widget.ListView;
 public class ListViewActivity extends AppCompatActivity {
 
     EditText m_editList;
-    Button m_submitList;
-    ListView m_listView;
+    Button m_btnSubmit;
+    ListView m_listExample;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview);
 
+        // Initialize the view variables, so we can use them later
         m_editList = findViewById(R.id.edit_list);
-        m_submitList = findViewById(R.id.button_submit_conversation);
+        m_btnSubmit = findViewById(R.id.button_submit_list);
+        m_listExample = findViewById(R.id.list_example);
 
-        m_submitList.setOnClickListener((v) -> {
-            m_listView = new ListView(this);
-            m_listView.setAdapter(new ArrayAdapter<String>(
+
+        // Setting the contents of the list view by the button press
+        // Remember, when we want a long
+        m_btnSubmit.setOnClickListener((v) ->
+            // The setAdapter method set the values of the ListView according to a given array
+            // The array is given as part of an "ArrayAdapter" object
+            // Which is constructed with the following parameters
+            m_listExample.setAdapter(new ArrayAdapter<String>(
+                    // The activity we are acting from/
                     this,
+                    // The ID of a layout containing the text element which will
+                    // construct each row. NOTE - First that is the *id* rather than
+                    // the layout itself, and second, that the file does not contain a
+                    // full layout, but rather just a single element (e.g TextView),
+                    // without any parents,
                     R.layout.listrow_listview_example,
-                    ",".split(String.valueOf(m_editList.getText()))));
+                    // The array to fill the ListView with.
+                    // Here, we just split the text in each comma,
+                    // so you can create your own lists and have them
+                    // show up in the view.
+                    String.valueOf(m_editList.getText()).split(","))
+            )
+        );
 
 
-        });
+
     }
 
 
